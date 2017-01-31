6e38e5b11e2b4660676545a14e4295318cba5dc2
pranavj1001 <pranavj1001@gmail.com>
2017-01-31 21:03:13 +0530

    add toggle play and mute functions

--- js/videoPlayerApp.js
+++ js/videoPlayerApp.js
@@ -8,4 +8,32 @@ videoPlayerApp.controller('VideoController', ['$scope', '$window', function($sco
     $scope.titleDisplay = $window.titleDisplay;
     $scope.videoDescription = $window.videoDescription;
     
+    $scope.videoPlaying = false; 
+    
+    $scope.togglePlay = function() { 
+        if($scope.videoDisplay.paused){
+            $scope.videoDisplay.play();
+            $scope.videoPlaying = true;
+            $('#playBtn').children("span").toggleClass("glyphicon-play", false);
+            $('#playBtn').children("span").toggleClass("glyphicon-pause", true);
+        }else{
+            $scope.videoDisplay.pause();
+            $scope.videoPlaying = false;
+            $('#playBtn').children("span").toggleClass("glyphicon-play", true);
+            $('#playBtn').children("span").toggleClass("glyphicon-pause", false);
+        }
+    };
+    
+    $scope.toggleMute = function() {
+        if($scope.videoDisplay.volume == 0.0){
+            $scope.videoDisplay.volume = 1.0;
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-up", true);
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", false);
+        }else{
+            $scope.videoDisplay.volume = 0.0;
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-up", false);
+            $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", true);
+        }
+    };
+    
 }]);
