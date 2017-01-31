36a34ee92621a70aa2e56c842eb3c089fcfad5c3
pranavj1001 <pranavj1001@gmail.com>
2017-01-31 21:23:56 +0530

    add logic to display time duration of the video and comments

--- js/videoPlayerApp.js
+++ js/videoPlayerApp.js
@@ -3,13 +3,21 @@ var videoPlayerApp = angular.module('videoPlayerApp', []);
 //Controller
 videoPlayerApp.controller('VideoController', ['$scope', '$window', function($scope, $window) {
     
+    //scope var for the main video element.
     $scope.videoDisplay = document.getElementById("videoOutput");
     $scope.videoSource = $window.videoSource;
     $scope.titleDisplay = $window.titleDisplay;
     $scope.videoDescription = $window.videoDescription;
     
-    $scope.videoPlaying = false; 
+    //scope var for the ng-hide functions.
+    //i.e. when the video plays hide some elements and vice versa.
+    $scope.videoPlaying = false;
     
+    //scope var for the time duration of the video.
+    $scope.currentTime;
+    $scope.totalTime;
+    
+    //toggle Play function.
     $scope.togglePlay = function() { 
         if($scope.videoDisplay.paused){
             $scope.videoDisplay.play();
@@ -24,6 +32,7 @@ videoPlayerApp.controller('VideoController', ['$scope', '$window', function($sco
         }
     };
     
+    //toggle Mute function.
     $scope.toggleMute = function() {
         if($scope.videoDisplay.volume == 0.0){
             $scope.videoDisplay.volume = 1.0;
@@ -35,5 +44,34 @@ videoPlayerApp.controller('VideoController', ['$scope', '$window', function($sco
             $('#muteBtn').children("span").toggleClass("glyphicon-volume-off", true);
         }
     };
+    
+    //initialize the Player.
+    $scope.initPlayer = function() {
+        $scope.currentTime = 0;
+        $scope.totalTime = 0;
+        //add some Event Listeners.
+        $scope.videoDisplay.addEventListener("timeupdate", $scope.updateTime, true);//this event is fired when the time indicated by the currentTime attribute has been updated.
+        $scope.videoDisplay.addEventListener("loadedmetadata", $scope.updateData, true);//this event is fired when the video is loaded.
+    };
+    
+    //function to set the totalTime duration of the video.
+    $scope.updateData = function(e) {
+        $scope.totalTime = e.target.duration;
+    };
+    
+    //function to update Time of the video.
+    $scope.updateTime = function(e) {
+        $scope.currentTime = e.target.currentTime;
+        $scope.updateLayout();
+    };
+
+    //function to make sure that updated Time changes get reflected on the main video Player 
+    $scope.updateLayout = function() {
+        if(!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
+    
+    $scope.initPlayer();
     
 }]);
