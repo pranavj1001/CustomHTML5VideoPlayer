51c0033eb9574be6fb6a33fdfcaf794daa2de241
pranavj1001 <pranavj1001@gmail.com>
2017-01-31 20:07:20 +0530

    bind data using angular js

--- js/videoPlayerApp.js
+++ js/videoPlayerApp.js
@@ -1,8 +1,11 @@
 var videoPlayerApp = angular.module('videoPlayerApp', []);
 
 //Controller
-videoPlayerApp.controller('VideoController', ['$scope', function($scope) {
+videoPlayerApp.controller('VideoController', ['$scope', '$window', function($scope, $window) {
     
-    
+    $scope.videoDisplay = document.getElementById("videoOutput");
+    $scope.videoSource = $window.videoSource;
+    $scope.titleDisplay = $window.titleDisplay;
+    $scope.videoDescription = $window.videoDescription;
     
 }]);
