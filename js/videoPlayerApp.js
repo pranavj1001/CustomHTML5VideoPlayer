4adbddb0d47d10839b5f2c658153697b92705426
pranavj1001 <pranavj1001@gmail.com>
2017-02-01 23:25:05 +0530

    add a fliter to format time display

--- js/videoPlayerApp.js
+++ js/videoPlayerApp.js
@@ -75,3 +75,16 @@ videoPlayerApp.controller('VideoController', ['$scope', '$window', function($sco
     $scope.initPlayer();
     
 }]);
+
+//Filter to display a more friendly time display
+videoPlayerApp.filter('time', function() {
+    
+    return function(seconds) {
+        
+        var hh = Math.floor(seconds / 3600), mm = Math.floor(seconds / 60) % 60, ss = Math.floor(seconds) % 60;
+        return hh + ":" + (mm < 10 ? "0" : "") + mm + ":" + (ss < 10 ? "0" : "") + ss;
+        
+    };
+    
+});
+
